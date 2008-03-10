a80e590b3e0bd41dfc989ab83cc26222f19288d1
bobbin.worker <bobbin.worker@b6b84664-0842-0410-b802-23714e523fbe>
2008-03-10 22:01:35 +0000

    moved key generation into ant build

--- genkey.bat
+++
@@ -1,9 +0,0 @@
-rem ****************************************************************************
-rem *** sample file that prepares to run the ant build up to the last step 
-rem ****************************************************************************
-rem set JAVA_HOME=c:/Program files/Java/jdk1.6.0_03
-rem set ANT_HOME=C:\Program Files\apache-ant-1.7.0 
-rem set PATH=%PATH%;%JAVA_HOME\bin;%ANT_HOME%\bin
-rem set KEYSTOREPASS=yourpassword
-
-keytool -genkey -keystore bw -alias signjar -storepass %KEYSTOREPASS% -keypass %KEYSTOREPASS% -dname "OU=Development, O=BobbinWork, C=NL"
