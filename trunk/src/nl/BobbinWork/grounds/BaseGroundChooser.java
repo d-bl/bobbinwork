b85f24d5a7d80b3ce3b168efa686d89eba2cb849
bobbin.worker <bobbin.worker@b6b84664-0842-0410-b802-23714e523fbe>
2008-03-30 20:09:15 +0000

    Grounds now runs both as applet and application; merged xml file for grounds and basic stitches; removed splash as it caused problems, costs bandwdith and solves hardly anything

--- trunk/src/nl/BobbinWork/grounds/BaseGroundChooser.java
+++ trunk/src/nl/BobbinWork/grounds/BaseGroundChooser.java
@@ -3,17 +3,27 @@ package nl.BobbinWork.grounds;
 import static nl.BobbinWork.bwlib.gui.Localizer.applyStrings;
 
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 import nl.BobbinWork.bwlib.gui.LocaleMenuItem;
 
 @SuppressWarnings("serial")
 public class BaseGroundChooser extends JMenu {
 	
+    private static final String ERROR_CAPTION = "loading"; //$NON-NLS-1$
+	private static final String BASIC_STITCHES = "nl/BobbinWork/diagram/xml/basicStitches.xml";
+	private static final String PROCESSING_INSTRUCTION = "<?.*?>"; //$NON-NLS-1$
+    private static final String END_TAG = "</diagram>"; //$NON-NLS-1$
+	private final String source = loadFile();
+
 	private static final int COUNT = 4;
-	private static final String PREFIX = "MenuGround_";
+	private static final String PREFIX = "MenuGround_"; //$NON-NLS-1$
 
 	public BaseGroundChooser (final ActionListener externalActionListener){
     	super();
@@ -21,12 +31,12 @@ public class BaseGroundChooser extends JMenu {
 
 		JMenuItem jMenuItem;
 		Object [][] data = {
-				{"flanders",60,60, 4},//$NON-NLS-1$
+				{"flanders",50,50, 4},//$NON-NLS-1$
 				{"spider",80,140, 6},//$NON-NLS-1$
 				{"vierge",80,80, 4},//$NON-NLS-1$
 				{"sGravenmoers",80,80,4}};//$NON-NLS-1$
 		for (Object[] item : data) {
-	        jMenuItem = new LocaleMenuItem(PREFIX+item[0]); //$NON-NLS-1$
+	        jMenuItem = new LocaleMenuItem(PREFIX+item[0]); 
 	        jMenuItem.setActionCommand(assemble((String)item[0],(Integer)item[1],(Integer)item[2], (Integer)item[3]));
 	        jMenuItem.addActionListener(externalActionListener);
 	        add(jMenuItem);
@@ -48,6 +58,25 @@ public class BaseGroundChooser extends JMenu {
 			}			
 		}
 		System.out.println(s);
-		return "<group pairs='1-16'>"+s+"</group>";
+		return source+"<group pairs='1-16'>"+s+"</group>"+END_TAG; //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	private String loadFile () {
+		InputStream stream = getClass().getClassLoader().getResourceAsStream(BASIC_STITCHES);
+        char[] cbuf = new char[65*1024];
+        try {
+			(new InputStreamReader(stream)).read(cbuf);
+			return new String(cbuf)
+			.replaceFirst(END_TAG+".*", "") //$NON-NLS-1$ //$NON-NLS-2$
+			.replaceFirst(PROCESSING_INSTRUCTION, ""); //$NON-NLS-1$
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(
+					this, 
+					e.getLocalizedMessage()+"\n", //$NON-NLS-1$
+			        ERROR_CAPTION, 
+			        JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
--- trunk/src/nl/BobbinWork/grounds/Grounds.java
+++ trunk/src/nl/BobbinWork/grounds/Grounds.java
@@ -1,19 +1,19 @@
 package nl.BobbinWork.grounds;
 
+import static java.awt.BorderLayout.CENTER;
+import static java.awt.BorderLayout.PAGE_START;
 import static javax.swing.JSplitPane.HORIZONTAL_SPLIT;
 import static nl.BobbinWork.bwlib.gui.Localizer.setBundle;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Date;
 import java.util.Locale;
 
 import javax.swing.JMenuBar;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import nl.BobbinWork.bwlib.gui.BWApplet;
 import nl.BobbinWork.bwlib.gui.HelpMenu;
@@ -33,14 +33,6 @@ public class Grounds extends BWApplet {
 	private static String caption = "Grounds"; // gets extended by the help menu  //$NON-NLS-1$  
 
     private static final String BUNDLE_NAME = "nl/BobbinWork/viewer/gui/labels"; //$NON-NLS-1$
-    private static final String DIAGRAM = "nl/BobbinWork/grounds/grounds.xml"; //$NON-NLS-1$
-    private static final String BASIC_STITCHES = "nl/BobbinWork/diagram/xml/basicStitches.xml"; //$NON-NLS-1$
-    private static final String ICON = "nl/BobbinWork/viewer/gui/bobbin.gif"; //$NON-NLS-1$
-
-    private static final String END_TAG = "</diagram>";
-	private final String source = 
-		loadFile(BASIC_STITCHES).replaceFirst(END_TAG,"")//$NON-NLS-1$
-		+ loadFile(DIAGRAM).replaceFirst("<diagram[^>]*>","");//$NON-NLS-1$//$NON-NLS-2$
 
     public Grounds () {
     	super (BUNDLE_NAME);
@@ -48,36 +40,54 @@ public class Grounds extends BWApplet {
     
 	public static void main(String[] args) {
 
-        BWApplet applet = new BWApplet(BUNDLE_NAME);
         if (args.length > 0) setBundle(BUNDLE_NAME, new Locale(args[0]));
-        applet.init();
-        wrapInApplicationFrame(applet,caption, ICON);
+        wrapInApplicationFrame(new Grounds(),caption);
     }
 
 	public void init() {
 
 		final DiagramPanel diagramPanel = new DiagramPanel();
 			
-		JMenuBar jMenuBar = new JMenuBar();
-		jMenuBar.add( createChooser(diagramPanel));
-        jMenuBar.add( createHelpMenu() );
-        setJMenuBar(jMenuBar);
-		
 		getContentPane().add(new SplitPane(//
-				50, // dividerPosition
+				150, // dividerPosition
                 HORIZONTAL_SPLIT, // orientation
-                jMenuBar, // left component of spiltPane
+                new Customizer(diagramPanel), // left component of spiltPane
                 new InteractiveDiagramPanel(diagramPanel, this)));	// right component
     }
+	
+	private class Customizer extends JPanel {
+		
+		Customizer (DiagramPanel mainDiagramPanel) {
 
-	private BaseGroundChooser createChooser(final DiagramPanel diagramPanel) {
+			super(new BorderLayout());
+			
+			final JPanel stitchChooser = new JPanel();
+			stitchChooser.setToolTipText("changing individual stitches is not yet implemented");//$NON-NLS-1$
+
+			JMenuBar jMenuBar = new JMenuBar();
+			jMenuBar.add( createChooser(mainDiagramPanel, stitchChooser));
+	        jMenuBar.add( createHelpMenu() );
+	        
+	        add( jMenuBar, PAGE_START );
+	        add( stitchChooser, CENTER );
+		}
+	}
+
+	private BaseGroundChooser createChooser(
+			final DiagramPanel mainDiagramPanel,
+			final JPanel stitchChooser) {
+
 		return new BaseGroundChooser( new ActionListener(){
+			
 			public void actionPerformed(ActionEvent e) {
-				diagramPanel.setPattern(loadDiagram(e.getActionCommand()));
+				mainDiagramPanel.setPattern(loadDiagram(e.getActionCommand()));
+				// TODO something like 
+				// partition(..).draw(stitchChoosooser.getGraphics(), true, false);
 			}});
 	}
 
 	private HelpMenu createHelpMenu() {
+		
 		HelpMenu helpMenu = new HelpMenu(this, YEARS,caption);
 		caption = helpMenu.getVersionedCaption();
 		return helpMenu;
@@ -86,39 +96,20 @@ public class Grounds extends BWApplet {
 	private Diagram loadDiagram(String choice) {
 		
 		try {
-			String s = source.replaceFirst(END_TAG, choice+END_TAG);
-			System.out.println(s);
-			Element root = (new TreeBuilder(false)).build(s);
+			Element root = (new TreeBuilder(false)).build(choice);
 			root = root.getOwnerDocument().getDocumentElement();
 			System.out.println(""+new Date());
 			TreeExpander.parse(root);
 			System.out.println(""+new Date());
 			return new Diagram(root);
 		} catch (Exception e) {
-	        showError(e);
+			JOptionPane.showMessageDialog(
+					this, 
+					e.getLocalizedMessage()+"\n", 
+			        "loading", //$NON-NLS-1$
+			        JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
 		}
 		return null;
-	}
-
-	private String loadFile (String file) {
-		InputStream stream = getClass().getClassLoader().getResourceAsStream(file);
-        char[] cbuf = new char[(int)new File(file).length()];
-        try {
-			(new InputStreamReader(stream)).read(cbuf);
-			return new String(cbuf).replaceFirst("<?.*?>", "");//$NON-NLS-1$//$NON-NLS-2$
-		} catch (IOException e) {
-	        showError(e);
-		}
-		return null;
-	}
-
-	private void showError(Exception e) {
-		
-		JOptionPane.showMessageDialog(
-				this, 
-				e.getLocalizedMessage()+"\n", 
-		        "loading", //$NON-NLS-1$
-		        JOptionPane.ERROR_MESSAGE);
-		e.printStackTrace();
 	}
 }
