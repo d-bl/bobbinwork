92a3a9c09df654e280db5f9a2c8be6b0109860a5
bobbin.worker <bobbin.worker@b6b84664-0842-0410-b802-23714e523fbe>
2009-04-30 17:52:58 +0000

    more tests in preparation for improved bounds, simplified the visualizer

--- trunk/JUnitTests/nl/BobbinWork/diagram/model/PointsInSwitches.java
+++ trunk/JUnitTests/nl/BobbinWork/diagram/model/PointsInSwitches.java
@@ -1,17 +1,38 @@
+/* PointsInSwitches.java Copyright 2009 by J. Falkink-Pol
+ *
+ * This file is part of BobbinWork.
+ *
+ * BobbinWork is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BobbinWork is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BobbinWork.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package nl.BobbinWork.diagram.model;
 
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 
 import nl.BobbinWork.diagram.xml.DiagramBuilder;
 import nl.BobbinWork.diagram.xml.XmlResources;
+import nl.BobbinWork.diagram.xml.expand.TreeExpander;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /** Positive tests that check the shape of bounding polygons. */
 @RunWith(Parameterized.class)
@@ -19,17 +40,16 @@ public class PointsInSwitches {
 	
 	private static XmlResources xr = null;
 	
-	private static final String BASIC_STITCHES = 
+	private static final String DIAGRAM_ROOT = 
 	"<diagram" +
-	"  xmlns='http://BobbinWork.googlecode.com/bw.xsd'" +
-	"  xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'" +
-	"  xsi:schemaLocation='http://BobbinWork.googlecode.com bw.xsd'" +
-	"  xmlns:xi='http://www.w3.org/2001/XInclude'" +
-	">" +
-	"  <xi:include href='basicStitches.xml'/>" +
-	"  <copy of='ctctc' pairs='1-2'/>" +
-	"</diagram>";
-	
+	" xmlns='http://BobbinWork.googlecode.com/bw.xsd'" +
+	" xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'" +
+	" xsi:schemaLocation='http://BobbinWork.googlecode.com bw.xsd'" +
+	" xmlns:xi='http://www.w3.org/2001/XInclude'" +
+	"><xi:include href='basicStitches.xml'/><group pairs='1-1'>";
+
+	private static final String DIAGRAM_ROOT_END = "</group></diagram>";
+
 	private static final String CTCTC= ""+
 	"<stitch id='ctctc' pairs='1-2'>"+
 	"<pair start='0,6.6' end='16.6,23.3'/>"+
@@ -42,9 +62,17 @@ public class PointsInSwitches {
 	"<twist bobbins='3-4'><back start='11.6,18.3' end='16.6,23.3'/><front start='16.3,15' c1='16.3,20' c2='16.3,20' end='11.6,25'/></twist>"+
 	"<cross bobbins='2-3'><back start='11.6,25' end='5,31.6'/><front start='5,25' end='11.6,31.6'/></cross>"+
 	"</stitch>";
-
+	
 	/** figure (b) on http://code.google.com/p/bobbinwork/wiki/MathChallenges#Highlighting_(groups_of)_stitches */
-	private static final String TCTC= ""+
+	private static final String TCTC_BASED_ON_BASIC_STITCHES = ""+
+	"<group pairs='1-8'>\n"+
+	"<copy of='l'   pairs='1-1'><rotate centre='15,15' angle='-45'/><move x='188' y='73'/></copy>"+
+    "<copy of='r'   pairs='2-2'><rotate centre='15,15' angle='75' /><move x='215' y='92'/></copy>"+
+    "<copy of='ctc' pairs='1-2'><rotate centre='10,10' angle='25' /><move x='195' y='85'/></copy>"+
+    "</group>";
+	
+	/** same drawing but differently grouped. TODO should get same bounds for whole thing */
+	private static final String TCTC_BUILDING_OWN_STITCHES= ""+
 	"<stitch pairs='1-2'>"+
 	"<pair end='206.69291333853582,99.65124117072072' start='201.61417332292834,85.69751765855855'/>"+
 	"<pair end='197.26120522699915,95.03248786503605' start='211.13040508681317,90.1350094068359'/>"+
@@ -52,7 +80,7 @@ public class PointsInSwitches {
 	"<back c1='186.45370132023479,92.66690475583121' c2='177.96841994599623,83.33309524416879' end='186.45370132023479,83.33309524416879' start='177.96841994599623,92.66690475583121'/>"+
 	"<front c1='186.45370132023479,83.33309524416879' c2='177.96841994599623,92.66690475583121' end='186.45370132023479,92.66690475583121' start='177.96841994599623,83.33309524416879'/>"+
 	"</twist>"+
-	"<twist bobbins='1-2'>"+
+	"<twist bobbins='3-4'>"+
 	"<back c1='243.19479216882343,98.28808519201685' c2='245.87635664134174,110.61404203032062' end='238.52788741299221,106.37140134320134' start='250.54326139717296,102.53072587913613'/>"+
 	"<front c1='238.52788741299221,106.37140134320134' c2='250.54326139717296,102.53072587913613' end='243.19479216882343,98.28808519201685' start='245.87635664134174,110.61404203032062'/>"+
 	"</twist>"+
@@ -73,80 +101,154 @@ public class PointsInSwitches {
 	"<front end='202.3375049510336,100.7097390583309' start='199.79227184603164,94.33700115055514'/>"+
 	"</cross>"+
 	"</stitch>";
-	
+
 	/*  To extract more examples from a diagram:
 	  
-	   	Document tmp = xr.parse(s);
-		TreeExpander.replaceCopyElements(tmp.getDocumentElement());
-		System.out.println( XmlResources.toXmlString(tmp));
+	   	System.out.println( XmlResources.toXmlString( parseEmbedded( s )));
 	*/
 	
     @Parameters
     public static Collection<Object[]> data() throws Exception {
     	xr = new XmlResources();
-    	MultiplePairsPartition tctc = createPart(TCTC);
-        MultiplePairsPartition ctctc1 = createPart(CTCTC);
-		MultiplePairsPartition ctctc2 = extractPart(BASIC_STITCHES);
+    	MultiplePairsPartition tctcBS = extractPart(TCTC_BASED_ON_BASIC_STITCHES);
+    	MultiplePairsPartition tctcOS = createPart(TCTC_BUILDING_OWN_STITCHES);
+		MultiplePairsPartition ctctcBS = extractPart("<copy of='ctctc' pairs='1-2'/>");
+		MultiplePairsPartition ctctcOS = createPart(CTCTC);
+		System.out.println( XmlResources.toXmlString( parseEmbedded( TCTC_BASED_ON_BASIC_STITCHES )));
+
+		
 		final Object[][] testCaseParameters = new Object[][] {
         		
-        		{ctctc1,8,3,7,3},
-        		{ctctc2,8,3,7,3}, // FIXME do we actually select the copied ctctc?
+        		{ctctcOS,ctctcBS,8,3,7,3}, 
         		
         		// just above end of front end in cross
-        		{tctc,202,98,202,98},
+        		{tctcOS,tctcBS,202,98,202,98},
         		
         		// start of front thread in cross
-        		{tctc,200,94,200,94}, // FIXME r269 introduced the problem
+        		{tctcOS,tctcBS,200,94,200,94}, // FIXME r269 introduced the problem
 
         		// end of back of left twist; checks merging polygons 
-        		{tctc,186,83,186,83}, // TODO merge not yet implemented
+        		{tctcOS,tctcOS,186,83,186,83}, // TODO merge not yet implemented
         /**/};
         return Arrays.asList(testCaseParameters);
     }
 
-    MultiplePairsPartition partition;
+    MultiplePairsPartition partP, partQ;
     int xa,xb,ya,yb;
 	
 	/** Creates a test object for a test case in data(). */
-	public PointsInSwitches(MultiplePairsPartition p, int xa, int ya, int xb, int yb){
-		this.partition = p;
+	public PointsInSwitches(MultiplePairsPartition variantP, MultiplePairsPartition variantQ, int xa, int ya, int xb, int yb){
+		this.partP = variantP;
+		this.partQ = variantQ;
 		this.xa = xa;
 		this.xb = xb;
 		this.ya = ya;
 		this.yb = yb;
 	}
 	
-	/** Executes the test for a case in data(). */
+	/** Executes tests for a case in data(). */
 	@Test
-	public void sameSwitches () throws Exception {
-		String sa = getSwitchAt(partition,xa,ya);
-		String sb = getSwitchAt(partition,xb,yb);
-		assertTrue (sa+" =/= "+sb,sa.equals(sb));
-		assertTrue ( "empty bounds for switch", ! sa.equals(""));
-		String b = partition.getBounds().toString();
-		assertTrue ( "no bounds for whole thing: "+b, b.matches(".*\\) \\(.*"));
+	public void variantP () throws Exception {
+		assertEqualSwitchBounds(partP);
 	}
+
+	/** Executes tests for a case in data(). */
+	@Test
+	public void variantQ () throws Exception {
+		assertEqualSwitchBounds(partQ);
+	}
+	
+	/** Executes a test for a case in data(). */
+	@Test
+	public void equalBounds () throws Exception {
+		String p = getBounds(partP);
+		String q = getBounds(partQ);
+		assertTrue (p+" =/= "+q,p.equals(q));
+	}
+
+	/** Executes a test for a case in data(). */
+	@Test
+	public void equalDrawables () throws Exception {
+		Iterator<Drawable> iteratorP = partP.threadIterator();
+		Iterator<Drawable> iteratorQ = partQ.threadIterator();
+		int count = 0;
+		while ( iteratorP.hasNext() && iteratorQ.hasNext() ) {
+			count++;
+			Drawable drawableP = iteratorP.next();
+			Drawable drawableQ = iteratorQ.next();
+			assertEqualShapes( drawableP, drawableQ);
+			assertEqualStyles(drawableP, drawableQ);
+		}
+		assertEqualNrOfDrawables(iteratorP, iteratorQ, count);
+	}
+
+	private void assertEqualNrOfDrawables(Iterator<Drawable> iteratorP,
+			Iterator<Drawable> iteratorQ, int count) {
+		String msg = "common number of elements = " + count + //
+		"; p.hasNext=" + iteratorP.hasNext() + //
+		"; q.hasNext=" + iteratorQ.hasNext();
+		assertTrue(msg,!iteratorP.hasNext() || !iteratorP.hasNext());
+	}
+
+	private void assertEqualStyles(Drawable drawableP, Drawable drawableQ) {
+		String styleP = drawableP.getStyle().toString();
+		String styleQ = drawableQ.getStyle().toString();
+		String msg = "styles are not queual: "+styleP +" =/= " + styleQ;
+		assertTrue( msg, styleP.equals(styleQ));
+	}
+	
+	private void assertEqualShapes(Drawable drawableP, Drawable drawableQ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private String getBounds(MultiplePairsPartition part) {
+		String b = part.getBounds().toString();
+		assertTrue ( "no bounds for whole thing: "+b, b.matches(".*\\) \\(.*"));
+		return b;
+	}
+	
+	private void assertEqualSwitchBounds(MultiplePairsPartition part) throws Exception {
+		String boundsA = getSwitchAt(part,xa,ya);
+		String boundsB = getSwitchAt(part,xb,yb);
+		assertTrue (boundsA+" =/= "+boundsB,boundsA.equals(boundsB));
+	}
+	
 	
 	/** Searches for a cross or twist in the partition at position (x,y).
 	 * @return the points of the bounding polygon of the found switch.
 	 */
 	private static String getSwitchAt(MultiplePairsPartition partition, int x, int y) throws Exception {
 		MultipleThreadsPartition switchAtXy = partition.getSwitchAt(x, y);
-		if (switchAtXy == null) throw new Exception("no cross or twist at ("+x+","+y+")\n" +
-				"bounding polygon for the whole thing: "+(partition.getBounds()));
-		return (switchAtXy.getBounds()).toString();
+		String msg = "no cross or twist at ("+x+","+y+")\n" +
+				"bounding polygon for the whole thing: "+(partition.getBounds());
+		assertTrue(msg,switchAtXy != null);
+		String bounds = (switchAtXy.getBounds()).toString();
+		assertTrue ( "empty bounds for switch", ! bounds.equals(""));
+		return bounds;
 	}
 
-	/** Creates a diagram object model from an XML string. */
+	/** Creates an object model from an XML string. */
 	private static MultiplePairsPartition createPart(String partition)
 			throws Exception {
-		return DiagramBuilder.createStitch(xr.parse(partition).getDocumentElement());
+		return DiagramBuilder.createStitch(parse(partition).getDocumentElement());
 	}
 	
-	/** Creates a diagram object model from an XML string. */
+	/** Creates an object model using the basic stitches. */
 	private static MultiplePairsPartition extractPart(String partition)
 	throws Exception {
-		Diagram diagram = DiagramBuilder.createDiagram(xr.parse(partition).getDocumentElement());
-		return (MultiplePairsPartition)diagram.getPartitions().get(0);
+		Element parsed = parseEmbedded(partition).getDocumentElement();
+		Diagram diagram = DiagramBuilder.createDiagram(parsed);
+		return (MultiplePairsPartition)diagram.getPartitions().get(1);
+	}
+
+	private static Document parseEmbedded(String s) throws Exception {
+		return parse(DIAGRAM_ROOT + s + DIAGRAM_ROOT_END);
+	}
+	
+	private static Document parse(String s) throws Exception {
+		Document parsed = xr.parse(s);
+		TreeExpander.replaceCopyElements(parsed.getDocumentElement());
+		return parsed;
 	}
 }
--- trunk/JUnitTests/nl/BobbinWork/diagram/model/Visualizer.java
+++ trunk/JUnitTests/nl/BobbinWork/diagram/model/Visualizer.java
@@ -1,3 +1,20 @@
+/* Visualizer.java Copyright 2009 by J. Falkink-Pol
+ *
+ * This file is part of BobbinWork.
+ *
+ * BobbinWork is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BobbinWork is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BobbinWork.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package nl.BobbinWork.diagram.model;
 
 import static java.awt.RenderingHints.KEY_ANTIALIASING;
@@ -12,51 +29,53 @@ import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JSplitPane;
 
 import nl.BobbinWork.diagram.gui.DiagramPanel;
 
 public class Visualizer {
-    private static final int TEST_CASE_NR = 0;
+	
 	private JFrame frame = new JFrame("visualizer"); 
 
 	static class MyPanel extends JPanel {
+		
 		private static final long serialVersionUID = 1L;
-		MultiplePairsPartition partition = null;
-		int x,y;
-		MyPanel(MultiplePairsPartition p, int x, int y) throws Exception {
-        	partition = p;
-        	this.x = x;
-        	this.y = y;
+		
+		private final Iterable<Drawable> shapes;
+		private final int scale;
+		
+		MyPanel(MultiplePairsPartition p, int scale) throws Exception {
+        	this.scale = scale;
+			shapes = p.getThreads();
         	setBackground(new Color(0xFFFFFF));
         }
 
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             Graphics2D g2 = (Graphics2D) g;
-            g2.scale(4, 4);
+            g2.scale(scale, scale);
             g2.setRenderingHint(KEY_INTERPOLATION, VALUE_INTERPOLATION_BILINEAR);
             g2.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
-            Iterable<Drawable> shapes = partition.getThreads();
 			DiagramPanel.paintPartitions (g2,shapes);
-			// TODO draw (x,y) + highLight (see DiagramPanel.highlightThreadAt())
-			//Shape highLight = partition.getSwitchAt(x,y).getBounds();
         }
 	}
 
-    public static void main (String[] args){
+    public Visualizer(MultiplePairsPartition part, int scale) throws Exception {
+    	MyPanel myPanel = new MyPanel( part, scale );
+    	frame.setSize(new Dimension(800, 300));
+		frame.getContentPane().add( myPanel);
+	}
+    
+    private static MultiplePairsPartition getPart(int caseNr, int variantNr)
+    throws Exception {
+    	Object[] testcases = PointsInSwitches.data().toArray();
+    	Object[] testCase = (Object[]) testcases[caseNr];
+    	return (MultiplePairsPartition) testCase[variantNr];
+    }
+
+	public static void main (String[] args){
     	try {
-    		Object[] testCase = (Object[]) PointsInSwitches.data().toArray()[TEST_CASE_NR];
-			MultiplePairsPartition partition = (MultiplePairsPartition) testCase[0];
-			Visualizer visualizer = new Visualizer();
-			MyPanel panelA = new MyPanel (partition, (Integer) testCase[1], (Integer) testCase[2]);
-			MyPanel panelB = new MyPanel (partition, (Integer) testCase[3], (Integer) testCase[3]);
-			JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panelA,panelB);
-			splitPane.setDividerLocation(400);
-			visualizer.frame.getContentPane().add(splitPane);
-			visualizer.frame.setSize(new Dimension(800, 300));
-			visualizer.frame.setVisible(true);
-			System.out.println("xxx");
+    		int caseNr = 1, variantNr = 0, scale = 2;
+			new Visualizer(getPart(caseNr, variantNr), scale).frame.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
