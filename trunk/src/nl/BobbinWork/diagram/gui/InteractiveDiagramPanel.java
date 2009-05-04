c3aab064d2606a9818ef41a3e5f2d2322d3ebae8
bobbin.worker <bobbin.worker@b6b84664-0842-0410-b802-23714e523fbe>
2009-05-04 19:56:07 +0000

    icons and hints for the new buttons

--- trunk/src/nl/BobbinWork/diagram/gui/InteractiveDiagramPanel.java
+++ trunk/src/nl/BobbinWork/diagram/gui/InteractiveDiagramPanel.java
@@ -9,8 +9,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.net.URL;
 
 import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -20,6 +22,7 @@ import javax.swing.JToolBar;
 import javax.xml.parsers.ParserConfigurationException;
 
 import nl.BobbinWork.bwlib.gui.CMenuBar;
+import nl.BobbinWork.bwlib.gui.Localizer;
 import nl.BobbinWork.bwlib.gui.PrintMenu;
 import nl.BobbinWork.diagram.model.Point;
 
@@ -61,22 +64,31 @@ public class InteractiveDiagramPanel extends JPanel {
         add(new JScrollPane(diagramPanel), CENTER);
 	}
 	
-    private class GetButton extends JButton {
-    	GetButton (String name) {
-    		super(name);
-            addActionListener(new ActionListener(){
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					if ( selected == null ) return;
-                    threadStyleToolBar.setStyleOfFrontThread//
-                         (diagramPanel.getThreadStyleAt((int)selected.getX(), (int)selected.getY()));
-				}});
+    private class MyButton extends JButton {
+    	MyButton (String imageFileName) {
+    		URL url = ThreadStyleToolBar.class.getResource(imageFileName);
+			setIcon(new ImageIcon(url));
+    		String baseFileName = imageFileName.replaceAll("\\..*", "");//$NON-NLS-1$//$NON-NLS-2$
+			Localizer.applyStrings(this,"ThreadStyle_"+baseFileName);//$NON-NLS-1$
     	}
     }
     
-    private class SetButton extends JButton {
-    	SetButton (String name) {
-    		super(name);
+    private class GetButton extends MyButton {
+    	GetButton () {
+    		super("pipette.gif");//$NON-NLS-1$
+    		addActionListener(new ActionListener(){
+    			@Override
+    			public void actionPerformed(ActionEvent e) {
+    				if ( selected == null ) return;
+    				threadStyleToolBar.setStyleOfFrontThread//
+    				(diagramPanel.getThreadStyleAt((int)selected.getX(), (int)selected.getY()));
+    			}});
+    	}
+    }
+    
+    private class SetButton extends MyButton {
+    	SetButton () {
+    		super("pinsel.gif");//$NON-NLS-1$
     		addActionListener(new ActionListener(){
     			@Override
     			public void actionPerformed(ActionEvent e) {
@@ -99,8 +111,8 @@ public class InteractiveDiagramPanel extends JPanel {
             add(new CMenuBar(menu));
             add(Box.createHorizontalGlue());
             add(toolBar);
-            add(new GetButton("get"));
-            add(new SetButton("apply"));
+            add(new GetButton());
+            add(new SetButton());
     	}
     }
 }
