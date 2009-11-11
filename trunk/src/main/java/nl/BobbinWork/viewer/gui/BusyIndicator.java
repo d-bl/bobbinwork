79432930c7cdd59eba7e696f5a34740d22412098
bobbin.worker <bobbin.worker@b6b84664-0842-0410-b802-23714e523fbe>
2009-11-11 17:35:33 +0000

    a starts for a busy indicator

---
+++ trunk/src/main/java/nl/BobbinWork/viewer/gui/BusyIndicator.java
@@ -0,0 +1,33 @@
+package nl.BobbinWork.viewer.gui;
+
+import java.net.URL;
+
+import javax.swing.*;
+
+import nl.BobbinWork.diagram.xml.DiagramRebuilder;
+
+/**
+ * TODO somehow show another colour during each time consuming step  
+ * in {@link BwDiagrams#createFileListener}
+ * and in {@link DiagramRebuilder#rebuild}
+ * The zero'th icon is a safe value for not busy.
+ * 
+ * simply setting an icon on a button like the others did not work
+ * possibly another thread is needed
+ * previous attempts to change the cursor was also buggy 
+ * */
+public class BusyIndicator
+{
+  protected static final Icon[] ICONS = initIcons();
+  private static Icon[] initIcons()
+  {
+    final Icon[] result = new ImageIcon[4];
+    for (int i=0;i<result.length;i++){
+      final URL url = BwDiagrams.class.getResource( "busy"+i+".gif" );
+      if (url != null)
+      result[i] = new ImageIcon(url);
+    }
+    return result;
+  }
+  
+}
