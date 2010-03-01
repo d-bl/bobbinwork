89cbc2d3aae362ba7da5d049f71a357f6331be1b
bobbin.worker <bobbin.worker@b6b84664-0842-0410-b802-23714e523fbe>
2010-03-01 08:02:49 +0000

    diagrams no longer embedded in Visio generated html pages

--- trunk/src/main/java/nl/BobbinWork/diagram/xml/expand/TreeExpander.java
+++ trunk/src/main/java/nl/BobbinWork/diagram/xml/expand/TreeExpander.java
@@ -90,8 +90,8 @@ public class TreeExpander
    * another via setUserData(CLONE_TO_ORPHAN) respective
    * setUserData(ORPHAN_TO_CLONE).
    * 
-   * {@link http://bobbinwork.googlecode.com/svn/trunk/doc/ModelObjects.html}
-   * illustrates how the presentation model will be able to manipulate the
+   * {@linkplain http://bobbinwork.googlecode.com/svn/trunk/doc/ModelObjects.html}
+   * illustrates how nodes of the presentation model can be linked to nodes in the
    * original document. getSourceObject() allows to change the 'of' attributes
    * in the original DOM-tree, [INDIRECT_]CLONE_TO_ORPHAN allows to create these
    * relations. ORPHAN_TO_CLONE allows to restore the original DOM-tree.
